70091dc41b4e1114146797f5035496bb0bd77fc3
Alex <honguttdc@gmail.com>
2022-11-26 22:00:57 -0800

    update hpa file, everything working on local Co-authored-by: dukebao <dukebao@users.noreply.github.com>

---
+++ misc/test_request.py
@@ -0,0 +1,25 @@
+import requests
+import random 
+
+url = 'http://localhost:3001/input_data?username=testing'
+
+
+height = random.randint(1, 100)
+weight = random.randint(1, 100)
+people = ['steven', 'james', 'joe', 'jim', 'jane', 'jill', 'jennifer', 'jessica', 'josh', 'joshua']
+
+
+def main():
+    while True:
+      data = {
+          "name": random.choice(people),
+          "height": f'{height}',
+          "weight": f'{weight}',
+          "gender": "F",
+          "age" : "123"
+          }
+      r = requests.post(url, json=data)
+      print(r)
+      
+if __name__ == "__main__":
+  main()
