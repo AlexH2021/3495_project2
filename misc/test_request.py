18624a3afa2cdf95eba183d704a697341a782bec
AlexH2021 <honguttdc@gmail.com>
2022-11-30 17:37:29 +0000

    b

--- misc/test_request.py
+++ misc/test_request.py
@@ -1,25 +1,28 @@
 import requests
 import random 
 
-url = 'http://34.102.113.153:3001/input_data?username=testing'
+url = 'http://35.235.89.62:3001/input_data?username=testing'
 
 
 height = random.randint(1, 100)
 weight = random.randint(1, 100)
-people = ['steven', 'james', 'joe', 'jim', 'jane', 'jill', 'jennifer', 'jessica', 'josh', 'joshua']
+people = ['steven', 'james', 'joe', 'jim', 'jane', 'jill', 'jennifer', 'jessica', 'josh', 'joshua',
+'stevn', 'jame', 'je', 'jm', 'jae', 'jil', 'jennfer', 'jesica', 'joh', 'josua']
+gender = ['f', 'm', 'female', 'male', 'none']
+age = random.randint(1,100)
 
 
-def main():
-    while True:
-      data = {
-          "name": random.choice(people),
-          "height": f'{height}',
-          "weight": f'{weight}',
-          "gender": "F",
-          "age" : "123"
-          }
-      r = requests.post(url, json=data)
-      print(r)
+def send_request():
+    data = {
+        "name": random.choice(people),
+        "height": f'{height}',
+        "weight": f'{weight}',
+        "gender": random.choice(gender),
+        "age" : f'{age}'
+        }
+    r = requests.post(url, json=data)
+    print(r)
       
 if __name__ == "__main__":
-  main()
+    while True:
+        send_request()
